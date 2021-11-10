7afd1ca731a21947eb1588249b9a7fc8580f2ac8
Gopi Krishna M <gopi.muktevi@gmail.com>
2021-11-10 22:59:18 +0530

    moved assets into src directory
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Site under construction</h3>
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+          <div className="md:flex">
+            
+          </div>
+        </div>
       </header>
     </div>
   );
