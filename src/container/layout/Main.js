aa7670851bcbcc0083e776de490d727796e95326
Gopi Krishna M <gopi.muktevi@gmail.com>
2021-11-15 19:49:59 +0530

    Adding some css and images into repo
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/container/layout/Main.js
+++ src/container/layout/Main.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { HelmetProvider } from "react-helmet-async";
+import ScrollToTop from "../../commons/ScrollToTop";
 
 const Main = ( props ) => {
     return(
         <HelmetProvider>
-            
+            <ScrollToTop/>
         </HelmetProvider>
     );
 }
