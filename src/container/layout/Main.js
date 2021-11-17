9171d227421a7cd908a1f41aed462360c0539a3e
Gopi Krishna M <gopi.muktevi@gmail.com>
2021-11-17 21:41:30 +0530

    Done with Site layout
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/container/layout/Main.js
+++ src/container/layout/Main.js
@@ -1,6 +1,10 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import Navigation from "../../commons/Navigation";
+import SideBar from "../../commons/SideBar";
 import ScrollToTop from "../../commons/ScrollToTop";
+
 
 const Main = ( props ) => {
     return(
@@ -10,7 +14,31 @@ const Main = ( props ) => {
                 {props.title && <title>{props.title}</title>}
                 <meta name="description" content={props.description} />
             </Helmet>
-            <div id="wrapper"></div>
+            <div id="wrapper">
+                <Navigation/>
+                <div id="main">
+                    {props.children}
+                </div>
+                {props.fullPage ? null : <SideBar/>}
+            </div>
         </HelmetProvider>
     );
 }
+
+Main.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node,
+    ]),
+    fullPage: PropTypes.bool,
+    title: PropTypes.string,
+    description: PropTypes.string,
+};
+    Main.defaultProps = {
+    children: null,
+    fullPage: false,
+    title: null,
+    description: "Oscarmild's personal website.",
+};
+
+export default Main;
