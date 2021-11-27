574706dc490d30ebbd0768613dc288bb9864f8fc
Gopi Krishna M <gopi.muktevi@gmail.com>
2021-11-27 19:49:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Main from './container/layout/Main';
 
 import './static/css/main.scss';
 const { PUBLIC_URL } = process.env;
-console.log(PUBLIC_URL);
+console.log(PUBLIC_URL); 
 
 function App() {
 	return (
